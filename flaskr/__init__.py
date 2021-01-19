9c2dcf12b8054a56b82e19ef7c21db7e75b20337
zuby <zubyjaved@gmail.com>
2021-01-19 15:48:55 -0800

    return user input in results page

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -1,12 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 def create_app(test_config=None):
     # creates & configures app
     app = Flask(__name__, instance_relative_config=True)
     
     # a simple page that says hello
-    @app.route('/hello')
-    def hello():
-        return 'Hello World!'
+    @app.route('/')
+    def index():
+        return render_template('index.html')
+
+    @app.route('/results', methods=['POST'])
+    def results():
+        return request.form['inputTxt']
 
     return app

0f94d617179068301c55813b5db25fce1670ddd9
zuby <zubyjaved@gmail.com>
2021-01-19 16:18:31 -0800

    return closest matching line from user input

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -1,4 +1,7 @@
 from flask import Flask, render_template, request
+import json
+from fuzzywuzzy import fuzz
+import textwrap
 
 def create_app(test_config=None):
     # creates & configures app
@@ -11,6 +14,26 @@ def create_app(test_config=None):
 
     @app.route('/results', methods=['POST'])
     def results():
-        return request.form['inputTxt']
+        text = request.form['inputTxt']
+        response = get_best_line(text)
+        output = "<br>INPUT : " + text + "<br>CLOSEST MATCH : " + response['line_text']
+        output += "<br>SPEAKER : " + response['speaker'] + '<br>SEASON : ' + str(response['season']) + '<br>EPISODE : ' + str(response['episode'])
+        return output
 
     return app
+
+def get_best_line(text):
+    with open('flaskr/static/office-script.json') as f:
+        data = json.load(f)
+        bestLine = data[0]
+        bestRatio = fuzz.ratio(text, bestLine['line_text'])
+
+        for line in data:
+            theLine = str(line['line_text'])
+            ratio = fuzz.ratio(text, theLine)
+
+            if ratio > bestRatio:
+                bestLine = line
+                bestRatio = ratio
+
+        return bestLine
